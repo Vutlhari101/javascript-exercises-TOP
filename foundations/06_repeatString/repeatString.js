b3e9f1a8087eaf8f871ac4a210c4a8194e8ef335
Vutlhari <excellmabunda3@gmail.com>
2026-03-14 23:22:24 +0200

    completed exercise

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if(num < 0) return "ERROR";
+    if(num == 1) return string;
+    let stringToReturn = "";
+    for(i = 0; i < num; i++){
+        stringToReturn += string;
+    }
+    return stringToReturn;
 };
 
 // Do not edit below this line
--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(sequence) {
+    let reversedWord = "";
+    for(let i = sequence.length - 1; i >= 0; i--){
+        reversedWord += sequence[i];
+    }
+    return reversedWord;
 };
 
 // Do not edit below this line
