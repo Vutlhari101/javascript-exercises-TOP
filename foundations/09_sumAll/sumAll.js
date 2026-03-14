a3cc302aa6c5607a6099758a30e4aa20097f9d52
Vutlhari <excellmabunda3@gmail.com>
2026-03-14 23:51:59 +0200

    sumAll solution

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function(int1, int2) {
+    // Check if both are numbers, are integers, and are non-negative
+    if (!Number.isInteger(int1) || !Number.isInteger(int2)) return "ERROR";
+    if (int1 < 0 || int2 < 0) return "ERROR";
+    //make int 1 the smaller number
+    if(int1 > int2){
+        const temp = int1;
+        int1 = int2;
+        int2 = temp;
+    }
 
+    let sum = 0;
+    for(let i = int1; i <= int2; i++){
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
