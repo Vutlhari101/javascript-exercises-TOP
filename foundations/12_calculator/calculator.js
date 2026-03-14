b8a97b89e98743cbcfc756b3932d9e2e6a5b1eb3
Vutlhari <excellmabunda3@gmail.com>
2026-03-15 00:24:06 +0200

    exercise 12

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,32 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  if(array.length == 0) return 0;
+	return array.reduce((total, curr) => total + curr);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  if(array.length == 0) return 0;
+  return array.reduce((total, curr) => total * curr);
 };
 
-const power = function() {
-	
+const power = function(base, exp) {
+	return base ** exp;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  if(n < 0) return "ERROR";
+	if(n == 0){
+    return 1;
+  }else{
+    return n * factorial(n - 1);
+  }
 };
 
 // Do not edit below this line
